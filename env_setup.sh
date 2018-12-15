3b1e1ca1ca689cdbe197d6461a4c1e36b71ffbb5
Unknown <fand.kanade@gmail.com>
2018-12-15 16:13:30 -0500

    Changed environment variable setup from double to single quotes

--- env_setup.sh
+++ env_setup.sh
@@ -4,10 +4,10 @@
 # Set file to owner access only and hide server access
 
 # setup server database configuration
-export DB_NAME=""
-export DB_USER=""
-export DB_PASSWORD=""
+export DB_NAME=''
+export DB_USER=''
+export DB_PASSWORD=''
 
 # postgres setup
-export DB_HOST="localhost"
-export DB_PORT="5432"
+export DB_HOST='localhost'
+export DB_PORT='5432'
