ed52e6e0b169849aeed46f44959cfb01442072c9
Ubuntu <ubuntu@ip-172-31-94-52.ec2.internal>
2018-12-15 20:23:07 +0000

    Added script to setup environment

---
+++ env_setup.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Environment setup for ES configuration
+# Set file to owner access only and hide server access
+
+# setup server database configuration
+export DB_NAME=""
+export DB_USER=""
+export DB_PASSWORD=""
+
+# postgres setup
+export DB_HOST="localhost"
+export DB_PORT="5432"
