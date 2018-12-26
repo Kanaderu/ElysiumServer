f8a74aa6acfefb4d968e7fdfea636af007abed12
David Fan <fand.kanade@gmail.com>
2018-12-26 18:14:29 -0500

    Added LaTex support with KaTex
    Added KaTex version 0.10.0
    Used wagtail-draftail-katex to implement and apply KaTex
    Added source files for KaTex from main page
        CSS/JS files added (may need to do some cleanup/organization of static files)
    
    https://github.com/gatensj/wagtail-draftail-katex
    https://katex.org

--- ElysiumServer/wagtail_hooks.py
+++ ElysiumServer/wagtail_hooks.py
@@ -1,14 +1,40 @@
+from django.conf import settings
 
 import wagtail.admin.rich_text.editors.draftail.features as draftail_features
 from wagtail.admin.rich_text.converters.html_to_contentstate import InlineStyleElementHandler
 from wagtail.core import hooks
 from django.templatetags.static import static
 from django.utils.html import format_html
+from draftjs_exporter.dom import DOM
+from wagtail.admin.rich_text.converters.contentstate_models import Entity
+from wagtail.admin.rich_text.converters.html_to_contentstate import AtomicBlockEntityElementHandler
+
+
+def katex_entity_decorator(props):
+    """
+    Takes Draft.js ContentState and converts it to HTML for saving in the Database.
+    """
+    return DOM.create_element('div', {
+        'data-katex-text': props['text'],
+    })
+
+
+class KaTeXEntityElementHandler(AtomicBlockEntityElementHandler):
+    """
+    Takes Database HTML and converts it to Draft.js ContentState for display in the editor.
+    """
+    mutability = 'MUTABLE'
+
+    def create_entity(self, name, attrs, state, contentstate):
+        return Entity('KATEX', 'IMMUTABLE', {
+            'text': attrs['data-katex-text'],
+        })
 
 
 @hooks.register('insert_editor_css')
 def editor_css():
     return format_html('<link rel="stylesheet" href="{}" />', static('css/all.css'))
+
 
 @hooks.register('insert_editor_css')
 def admin_editor_css():
@@ -75,6 +101,39 @@ def register_strikethrough_feature(features):
     db_conversion = {
         'from_database_format': {tag: InlineStyleElementHandler(type_)},
         'to_database_format': {'style_map': {type_: tag}},
+    }
+
+    features.default_features.append(feature_name)
+    features.register_converter_rule('contentstate', feature_name, db_conversion)
+
+
+@hooks.register('register_rich_text_features')
+def register_katex_feature(features):
+    feature_name = 'katex'
+    type_ = 'KATEX'
+
+    control = {
+        'type': type_,
+        'label': '𝐊',
+        'description': 'KaTeX',
+    }
+
+    features.register_editor_plugin(
+        'draftail', feature_name, draftail_features.EntityFeature(control,
+            js=[static('js/katex.min.js'),
+                static('js/wagtail_draftail_katex.js'),
+            ],
+            css={
+                'all': [
+                    static('css/katex.min.css')
+            ]
+}
+        )
+    )
+
+    db_conversion = {
+        'from_database_format': {'div[data-katex-text]': KaTeXEntityElementHandler()},
+        'to_database_format': {'entity_decorators': {type_: katex_entity_decorator}},
     }
 
     features.default_features.append(feature_name)
